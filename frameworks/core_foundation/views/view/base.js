af45733d695e5f709daaa5b066c410b016a01d39
Jonathan Alley <AlleyJ@seapine.com>
2017-02-03 18:11:51 -0500

    Adding Touch Scrolling to SC.TemplateView 2

--- frameworks/core_foundation/views/view/base.js
+++ frameworks/core_foundation/views/view/base.js
@@ -1,4 +1,22 @@
 sc_require('mixins/delegate_support');
 
 /** @class */
-SC.CoreView = SC.Responder.extend(SC.DelegateSupport);
+SC.CoreView = SC.Responder.extend(SC.DelegateSupport, {
+  /**
+   * If touch scrolling is enabled on this view.
+   * @type {Boolean}
+   */
+  touchScrollingEnabled: true,
+
+  /**
+   * Returns if touch scrolling should be allowed on this view.
+   * Checks the touchScrollingEnabled property on this view and the
+   * allowTouchScrolling property of our parent view.
+   *
+   * @returns {Boolean} See description
+   */
+  allowTouchScrolling: function () {
+    var parentView = this.get('parentView');
+    return this.get('touchScrollingEnabled') && (parentView ? parentView.get('allowTouchScrolling') : true);
+  }.property('touchScrollingEnabled', 'parentView.allowTouchScrolling').cacheable()
+});
